13adfcfc94837e63e10835502c6d8480916ce93c
yakki-tori <chuxuantong1@gmail.com>
2021-07-05 17:26:27 +1000

    add

--- query.py
+++ query.py
@@ -3,11 +3,9 @@ import pandas as pd
 
 @route('/hello/<id>')
 def index(id):
-    datapath= "/Users/chuxuan/Desktop/domodata.csv"
+    datapath= "domodata.csv"
     data = pd.read_csv(datapath)
     d = data.loc[data['id']==int(id)]
-
-
     return template('<b>{{dress}}</b>', dress=d['dress'].values)
 
 run(host='localhost', port=8080)
